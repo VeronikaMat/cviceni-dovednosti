f564c0dcc9be8f5c0ac16cded9814932b2bb2788
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-05 14:48:22 +0100

    add code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Function to update skill level slider
+function updateSkill(sliderId, value) {
+    const slider = document.getElementById(sliderId);
+    slider.value = value;
+}
+
+// Ask user to input skill levels
+const htmlSkill = prompt("Please enter your HTML skill level (0-100):");
+const cssSkill = prompt("Please enter your CSS skill level (0-100):");
+const jsSkill = prompt("Please enter your JavaScript skill level (0-100):");
+
+// Update skill sliders on the page
+updateSkill("html-slider", htmlSkill);
+updateSkill("css-slider", cssSkill);
+updateSkill("js-slider", jsSkill);
+
+
+
 console.log('funguju!');
