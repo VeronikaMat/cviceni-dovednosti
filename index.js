87dcbea74ad5cecd13ff5fb7ce013c53a0306aff
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-09 15:11:03 +0100

    reapair js code, vytvoreni const

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function updateSkill(skillId, value) {
-    const dovednost = document.querySelector(skillId)
-    if (dovednost===null) return 
-    
-    //if (!dovednost) return=Proto if (!dovednost) return; v podstatě znamená "pokud není nalezen žádný prvek se specifikovaným ID, okamžitě ukončete funkci." 
-    //Je to způsob, jak řešit případ, kdy požadovaný prvek není nalezen, 
-    //což zabrání dalšímu vykonávání kódu a potenciálním chybám souvisejícím s pokusem o přístup k vlastnostem nebo metodám neexistujícího prvku.
+const updateSkill=(skillId, value)=> {
 
-
-    const progressPrvku = skillElement.querySelector('.skill__progress')
-    const hodnotaElement = skillElement.querySelector('.skill__value')
+    const progressPrvku = skillElement.querySelector(`${skillId} .skill__progress`)
+    const hodnotaElement = skillElement.querySelector(`${skillId} .skill__value`)
 
     if (progressPrvku) {
         progressPrvku.style.width = value + "%"
@@ -19,7 +12,7 @@ function updateSkill(skillId, value) {
     }
 }
 
-
+const updateSkills = () => {
 // Zeptej se uživatele aby vložil svůj skill levels
 const html = Number(prompt("Vyjáři svoji úroveň HTML (0-100):"))
 const css = Number(prompt("Vyjáři svoji úroveň CSS (0-100):"))
@@ -29,5 +22,7 @@ const js = Number(prompt("Vyjáři svoji úroveň JavaScript (0-100):"))
 updateSkill("skill1", html)
 updateSkill("skill2", css)
 updateSkill("skill3", js)
-
+}
 console.log('funguju!')
+
+
